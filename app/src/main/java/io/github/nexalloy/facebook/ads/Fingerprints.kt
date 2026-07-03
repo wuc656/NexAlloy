f6c288ca4d67e30534ea71e690007c47cc98ade3
彥東 <wuc656@gmail.com>
2026-07-03 17:14:02 +0800

    codex fb fix

--- app/src/main/java/io/github/nexalloy/facebook/ads/Fingerprints.kt
+++ app/src/main/java/io/github/nexalloy/facebook/ads/Fingerprints.kt
@@ -2,30 +2,29 @@ package io.github.nexalloy.facebook.ads
 
 import io.github.nexalloy.morphe.findMethodDirect
 
-// Match Runnable.run() in AdBucketDataSourceUtil$attemptAdsInsertion$1
-val adsInsertionFingerprint = findMethodDirect {
+private const val REDEX_ORIGINAL_NAME_FIELD = "__redex_internal_original_name"
+
+private fun runMethod(originalName: String) = findMethodDirect {
     findClass {
         matcher {
-            className("AdBucketDataSourceUtil\$attemptAdsInsertion")
+            addInterface("java.lang.Runnable")
+            addField {
+                name(REDEX_ORIGINAL_NAME_FIELD)
+                type("java.lang.String")
+            }
+            addEqString(originalName)
         }
     }.findMethod {
         matcher {
-            returnType = "void"
-            name = "run"
+            name("run")
+            returnType("void")
+            paramTypes()
         }
     }.single()
 }
 
-// Match Runnable.run() in AdBucketDataSourceUtil$attemptFetchMoreAds$1
-val fetchMoreAdsFingerprint = findMethodDirect {
-    findClass {
-        matcher {
-            className("AdBucketDataSourceUtil\$attemptFetchMoreAds")
-        }
-    }.findMethod {
-        matcher {
-            returnType = "void"
-            name = "run"
-        }
-    }.single()
-}
+// Match Runnable.run() in AdBucketDataSourceUtil$attemptAdsInsertion$1.
+val adsInsertionFingerprint = runMethod("AdBucketDataSourceUtil\$attemptAdsInsertion\$1")
+
+// Match Runnable.run() in AdBucketDataSourceUtil$attemptFetchMoreAds$1.
+val fetchMoreAdsFingerprint = runMethod("AdBucketDataSourceUtil\$attemptFetchMoreAds\$1")
