4a22bc225acb075abfedf1a94d881d5a97cb11f1
彥東 <wuc656@gmail.com>
2026-07-03 15:49:31 +0800

    FB test

--- app/src/main/java/io/github/nexalloy/facebook/ads/Fingerprints.kt
+++ app/src/main/java/io/github/nexalloy/facebook/ads/Fingerprints.kt
@@ -2,30 +2,30 @@ package io.github.nexalloy.facebook.ads
 
 import io.github.nexalloy.morphe.findMethodDirect
 
-// Match methods in AdBucketDataSourceUtil-related classes
-// These classes contain the ad insertion/fetch logic and have distinctive field names
+// Match Runnable.run() in AdBucketDataSourceUtil$attemptAdsInsertion$1
 val adsInsertionFingerprint = findMethodDirect {
     findClass {
         matcher {
-            className("AdBucketDataSourceUtil")
+            className("AdBucketDataSourceUtil\$attemptAdsInsertion")
         }
     }.findMethod {
         matcher {
             returnType = "void"
             name = "run"
         }
-    }.first()
+    }.single()
 }
 
+// Match Runnable.run() in AdBucketDataSourceUtil$attemptFetchMoreAds$1
 val fetchMoreAdsFingerprint = findMethodDirect {
     findClass {
         matcher {
-            className("AdBucketDataSourceUtil")
+            className("AdBucketDataSourceUtil\$attemptFetchMoreAds")
         }
     }.findMethod {
         matcher {
             returnType = "void"
             name = "run"
         }
-    }.last()
+    }.single()
 }
